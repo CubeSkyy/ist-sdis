c4e6d506484b7fa6eee6829e4cee0c50691032d1
Miguel Coelho <miguelmendescoelho@tecnico.ulisboa.pt>
2019-04-05 01:14:06 +0100

    Added hub.ctrlInitFood

---
+++ hub-ws/src/main/java/com/forkexec/hub/domain/HubFoodInit.java
@@ -0,0 +1,27 @@
+package com.forkexec.hub.domain;
+
+public class HubFoodInit {
+
+    protected HubFood food;
+    protected int quantity;
+
+
+    public HubFood getFood() {
+        return food;
+    }
+
+
+    public void setHubFood(HubFood value) {
+        this.food = value;
+    }
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int value) {
+        this.quantity = value;
+    }
+
+}
