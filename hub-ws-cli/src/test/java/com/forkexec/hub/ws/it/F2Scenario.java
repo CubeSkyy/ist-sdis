d47428e424d6b2bd2e107c936c96b0fd46b55a9c
CatarinaPedreira <catarina.pedreira@tecnico.ulisboa.pt>
2019-05-03 16:18:25 +0100

    f2Scenario

---
+++ hub-ws-cli/src/test/java/com/forkexec/hub/ws/it/F2Scenario.java
@@ -0,0 +1,51 @@
+package com.forkexec.hub.ws.it;
+
+
+import com.forkexec.hub.ws.InvalidCreditCardFault_Exception;
+import com.forkexec.hub.ws.InvalidInitFault_Exception;
+import com.forkexec.hub.ws.InvalidMoneyFault_Exception;
+import com.forkexec.hub.ws.InvalidUserIdFault_Exception;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class F1Scenario extends BaseIT {
+
+    @Test
+    public void success() throws InvalidUserIdFault_Exception, InvalidInitFault_Exception, InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception {
+
+        client.ctrlInitUserPoints(DEFAULT_INITIAL_BALANCE);
+        System.out.println("Definir o default inicial balance para: " + DEFAULT_INITIAL_BALANCE);
+
+        client.activateAccount(VALID_EMAIL);
+        System.out.println("Criada conta de email: " + VALID_EMAIL);
+
+        int amount = client.accountBalance(VALID_EMAIL);
+        Assert.assertEquals(amount, DEFAULT_INITIAL_BALANCE);
+        System.out.println("Conta criada com um balanço de: " + amount);
+
+
+        int add = 50;
+        client.loadAccount(VALID_EMAIL, add, VALID_FAKE_CC_NUMBER);
+        System.out.println("É feito um pedido de carregar a conta com: " + add + " euros (5500 pontos).");
+        int pointsAdded = 5500;
+        amount = client.accountBalance(VALID_EMAIL);
+        Assert.assertEquals(amount, DEFAULT_INITIAL_BALANCE + pointsAdded);
+        System.out.println("Novo saldo da conta: " + amount);
+
+        sleep(10);
+        System.out.println("Um dos servidores foi desligado");
+
+        add = 100;
+        client.loadAccount(VALID_EMAIL, add, VALID_FAKE_CC_NUMBER);
+        System.out.println("É feito um pedido de carregar a conta com: " + add + " euros (11000 pontos).");
+
+        int pointsAdded = 11000;
+        amount = client.accountBalance(VALID_EMAIL);
+        Assert.assertEquals(amount, 5500 + pointsAdded);
+        System.out.println("Novo saldo da conta: " + amount);
+
+        System.out.println("Exiting.");
+
+
+    }
+}
